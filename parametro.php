22d96bdf284690f6035b2f0bfdf95b63a54bc081
abigailavila <135040422+abigailavila@users.noreply.github.com>
2023-08-01 12:21:24 -0300

    parametro

---
+++ parametro.php
@@ -0,0 +1,12 @@
+<?php
+// Obtén los parametos de la solitud
+$parametro1 = $_GET['parametros1'];
+$parametro1 = $_GET['parametros2'];
+
+//Realiza las acciones necesarias con los parametros recibidos
+// Por ejemplo, puedes imprimir los valores en la pantalla
+echo "Parametro 1: " . $parametro1 . "<br>";
+echo "Parametro 2: " . $parametro2 . "<br>";
+    $resultado = $parametro1 + $parametro2;
+    echo "El resultado de la suma es". $resultado;
+?>    
